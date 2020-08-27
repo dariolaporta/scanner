4457b1c2a506f865a54268682514b0f9e6d30617
Dario <dario.laporta@temera.it>
2020-08-27 10:39:01 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Scanner',
+    version='1.0',
+    author='Dario Laporta',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[
+        'pyfiglet~=0.8.post1'
+    ]
+)
